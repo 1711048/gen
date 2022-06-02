c5d932fb5f70c20e10529360d953cfbb9ca8bd7c
1711048 <104405334+1711048@users.noreply.github.com>
2022-06-02 10:10:56 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
-basic.showLeds(`
-    . . # . .
-    . . # . .
-    . . # . .
-    . . . . .
-    . . # . .
-    `)
+music.playMelody("C D E F G A B C5 ", 120)
 basic.forever(function () {
-	
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
 })
